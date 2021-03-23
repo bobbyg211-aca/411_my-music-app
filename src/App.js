02caa2aef083ae1da4193f935b0582e59fb98ca0
bobbyg211 <robertdg96@gmail.com>
2021-03-23 18:08:35 -0400

    System component import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import SystemNotifications from "./components/SystemNotifications";
 
 function App() {
   return (
     <div>
       <Dashboard />
+      <SystemNotifications />
     </div>
   );
 }
